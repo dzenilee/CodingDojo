f0d4a8de1aec0998650232c0a67eb1293a8fcc65
dzenilee <dzenilee@gmail.com>
2017-03-15 22:25:57 -0700

    added i_do_declare.js assignment

---
+++ WebFundamentals/Javascript/i_do_declare.js
@@ -0,0 +1,29 @@
+// Declare variables of the following types:
+// 4 Numbers
+// 3 Strings
+// 2 Booleans
+// 1 Undefined Variable
+// Give the variables descriptive names (ex: string1 and string2)
+// Print each one to the console with a description (ex: console.log("The First String:", string1);)
+// Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)
+
+
+var num_one = 15;
+var num_two = 333;
+var num_three = 391.1;
+var num_four = -21;
+
+var string1 = 'hello world';
+var string2 = 'hola mundo';
+var string3 = 'como estas';
+
+var boolean1 = true;
+var boolean2 = false;
+
+var last_one = undefined;
+
+var var_names = [num_one, num_two, num_three, num_four, string1, string2, string3, boolean1, boolean2, last_one];
+
+for (i = 0; i < var_names.length; i++){
+  console.log(var_names[i]);
+}
