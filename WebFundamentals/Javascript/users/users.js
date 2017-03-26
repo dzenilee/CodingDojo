81de6433adcca81e6df195f6401eabf1eefaf2d1
dzenilee <dzenilee@gmail.com>
2017-03-26 16:34:53 -0700

    modified users.js

--- WebFundamentals/Javascript/users/users.js
+++ WebFundamentals/Javascript/users/users.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $('button').click(function(){
     var first = $('#first_name').val();
+    // var first = document.getElementById('first_name').value(); //throws error
     var last = $('#last_name').val();
     var email = $('#email').val();
     var contact = $('#contact').val();
     $('tbody').append('<tr><td>' + first + '</td><td>' + last + '</td><td>' + email + '</td><td>' + contact + '</td></tr>');
     return false;
   });
-}); 
+});
