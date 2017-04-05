2b014a141f9dd08efaff8abe195cd1bd2725c494
dzenilee <dzenilee@gmail.com>
2017-04-04 22:32:48 -0700

    added Python exercises

---
+++ Python/remove_blanks.py
@@ -0,0 +1,17 @@
+# Create a function that, given a string, returns all of that string's contents but without blanks.
+
+#declare a function with a name
+
+#test case for input that we assign to a variable
+
+#parse through the string and find all the characters that are blanks
+
+#python
+def remove_blanks(text):
+    new_string = ""
+    for char in text:
+        if char != " ":
+            new_string += char
+    print new_string
+
+remove_blanks("I love coding!")
