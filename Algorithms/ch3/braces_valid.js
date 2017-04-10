0b288aa27c168e42353f3c70dbd027549c8d326f
dzenilee <dzenilee@gmail.com>
2017-04-09 17:33:13 -0700

    modified braces_valid algo

--- Algorithms/ch3/braces_valid.js
+++ Algorithms/ch3/braces_valid.js
@@ -1,41 +1,30 @@
 //Given a sequence of parentheses, braces, and brackets, determine whether it is valid. Example:
 
-var txt1 = "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!" // true.
+var txt1 = "W(a{ts[}o(n{ c}o)m]e )h[e{r}e]!" // true (should this be false?) book typo?
 var txt2 = "D(i{a}l[ t]o)n{e" // false.
 var txt3 = "A(1)s[O(n]0{t) 0}k"  // false
 
-// function bracesValid(str){
-//   var left_paren = '('
-//   var right_paren = ')'
-//   var left_brace = '{'
-//   var right_brace = '}'
-//   var left_bracket = '['
-//   var right_bracket = ']'
-//
-//   var parens = [ left_paren, right_paren ];
-//   var braces = [ left_brace, right_brace ];
-//   var brackets = [ left_bracket, right_bracket ];
-//
-//   for (var i = 0; i < str.length; i++){
-//     for (var j = 0; j < str.length; j++){
-//       if (str[i] == parens[j]){
-//         console.log(str[i]);
-//       }
-//     }
-//   }
-// }
-
-// bracesValid(txt1)
-
 function bracesValid(str){
-  var bracketCounter, braceCounter, parenCounter == 0;
+  var check = [];
   for (var i = 0; i < str.length; i++){
-    if (str[i] == "("){
-      parenCounter++;
-    } else if (str[i] == "["){
-      bracketCounter++;
-    } else if (str[i] == "{"){
-      braceCounter++;
-    } 
+    if (str[i] == "(" || str[i] == "[" || str[i] == "{"){
+      check.push(str[i]);
+    } else if (str[i] == ")" || str[i] == "]" || str[i] == "}"){
+      if (str[i] == ")" && check[check.length-1] == "(" || str[i] == "}" && check[check.length-1] == "{" || str[i] == "]" && check[check.length-1]== "["){
+        check.pop(); //if closed paren matches open paren, pop open paren
+      } else {
+        return false;
+      }
+    }
   }
+  console.log(check);
+  if (check.length){
+    return false;
+  }
+  return true;
 }
+
+console.log(bracesValid("{([])}"));
+// console.log(bracesValid(txt1));
+// console.log(bracesValid(txt2));
+// console.log(bracesValid(txt3));
