3ad93650f79fb0b33be97e8ed8492d28e5e5d62a
dzenilee <dzenilee@gmail.com>
2017-04-10 00:08:51 -0700

    modified is_palindrome.js

--- Algorithms/ch3/is_palindrome.js
+++ Algorithms/ch3/is_palindrome.js
@@ -1,36 +1,40 @@
 // Create a function that returns a boolean whether the string is a strict palindrome. For "a x a" or "racecar", return true.
 
-
 //Do not ignore spaces, punctuation and capitalization: if given "Dud" or "oho!", return false.
 function isPalindromeStrict(str){
   var reversedStr = ""
   for (var i = str.length-1; i >= 0; i--){
     reversedStr += str[i];
   }
-  console.log(reversedStr);
   if (reversedStr == str){
     return true;
   }
   return false;
 }
-
-// console.log(isPalindromeStrict("racecar")); //true
-// console.log(isPalindromeStrict("oho!")); //false
+console.log(isPalindromeStrict("racecar")); //true
+console.log(isPalindromeStrict("oho!")); //false
 
 //DO ignore spaces, punctuation and capitalization: if given "Dud" or "oho!", return true.
-//still working
 function isPalindrome(str){
-  var reversedStr = "";
-  // var ignore = ["!", ",", ".", "!", " "];
-  str.toLowerCase();
-  for (var i = str.length-1; i >= 0; i--){
-      reversedStr += str[i];
-  }
-  for (var j = 0; j < str.reversedStr; j++){
-    if (reversedStr[j] != ','){
-      return reversedStr;
+  var newStr = "";
+  var ignore = ["!", ",", ".", "!", " "];
+  str = str.toLowerCase();
+  for (var i = 0; i < str.length; i++){
+    if (ignore.indexOf(str[i]) > -1){
+      continue;
+    } else {
+      newStr += str[i];
     }
   }
+  var reversedStr = "";
+  for (var j = newStr.length-1; j >= 0; j--){
+    reversedStr += newStr[j];
+  }
+  if (reversedStr == newStr){
+    return true;
+  }
+  return false;
 }
 
-console.log(isPalindrome("race,car"));
+console.log(isPalindrome("Rac.ecar.")); //true
+console.log(isPalindrome("oho!")); //true
