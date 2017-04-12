858b139fa4eb65c3033df631724e27b88e626747
dzenilee <dzenilee@gmail.com>
2017-04-11 22:08:16 -0700

    added dojo_survey

---
+++ Python/dojo_survey/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#this route handles form submission
+@app.route('/result', methods=['POST'])
+def create_user():
+    print "post info"
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template('result.html', name = name, location = location, language = language, comment= comment)
+
+app.run(debug=True) #run our server
