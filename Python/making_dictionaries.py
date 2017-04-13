4295171169ad1236e00cc386e4814af40e1c1424
dzenilee <dzenilee@gmail.com>
2017-04-12 21:09:12 -0700

    modified making_dictionaries.py

--- Python/making_dictionaries.py
+++ Python/making_dictionaries.py
@@ -4,15 +4,16 @@ name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
 favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
 
 def make_dict(arr1, arr2):
-  zipped = zip(arr1, arr2)
-  return dict(zipped) 
+  zipped = zip(arr1, arr2) # list of key-value tuples
+  return dict(zipped) # list to dictionary
 
 # harder way
 def make_dict2(arr1, arr2):
     new_dict = {}
     for key in arr1:
-        key_index = arr1.index(key)
+        key_index = arr1.index(key) # stores the index of arr1's key
         new_dict[key] = arr2[key_index]
     return new_dict
 
-print make_dict2(name, favorite_animal)
+print name.index('Eli') #1
+print make_dict2(name, favorite_animal) #{'Eli': 'cat', 'Shane': 'dolphins', 'Brendan': 'giraffe', 'Amy': 'ticks', 'Pariece': 'spider', 'Oscar': 'llamas', 'Anna': 'horse'}
