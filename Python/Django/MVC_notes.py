8d2ecc6165a256bab68e21e850d6f42302cd7db3
dzenilee <dzenilee@gmail.com>
2017-05-22 20:38:12 -0700

    added MVC notes

---
+++ Python/Django/MVC_notes.py
@@ -0,0 +1,49 @@
+"""
+MVC
+
+In Django:
+
+Model (Model)
+View (Templates)
+Controller (Views)
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Model: anything that deals with our DB (Model)
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+* Database
+* Calls to the DB
+* server connection file
+* Validation (validate in the area where the info is housed)
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+View: anything that deals with view of page (like templates) (Templates)
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+* Templates
+    HTML
+    Static files
+        CSS
+* Jinja Templating (Django Templating Engine)
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+Controller: handles logic (Views)
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+* Importing modules
+    Flask
+    Secret Key
+    render_template
+    (Models)
+* Declaration of server application
+* Running application (app.run)
+* Methods to do stuff
+    render template
+    redirect
+    talk to DB
+    set session
+
+-------------------------------
+
+Routing
+    Index routing
+    Advanced routing
+
+"""
