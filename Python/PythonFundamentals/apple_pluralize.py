8f4ddef5b3caae2ea7352007f107964abe01da5a
dzenilee <dzenilee@gmail.com>
2018-07-11 21:56:12 -0700

    add apple_pluralize.py

--- Python/PythonFundamentals/apple_pluralize.py
+++ Python/PythonFundamentals/apple_pluralize.py
@@ -1,17 +1,17 @@
 def pluralize(singular_arr):
     plural_arr = []
-    vowel = ["a", "i", "u"]
     coronal = ["s", "r"]
     for singular in singular_arr:
         if singular[-2:] == "ie":
-            if len(singular) >= 3 and singular[-3] in coronal:
+            if singular[-3] in coronal:
                 plural_arr.append(singular+"En")
             else:
                 plural_arr.append(singular+"n")
-        if singular[-1:] in vowel:
-            plural_arr.append(singular+"'s")
         else:
-            plural_arr.append(singular+"s")
+            if singular[-1] in ["a", "i", "u"] and singular[-2:] != "ou":
+                plural_arr.append(singular+"'s")
+            else:
+                plural_arr.append(singular+"s")
     print plural_arr
 
 singular_arr = ["berie", "kasie", "karie", "sona", "soni", "sonu", "sonou", "sone"]
