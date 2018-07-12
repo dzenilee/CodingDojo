bdd5155e723db876a0df3d739c8485cdb825098a
dzenilee <dzenilee@gmail.com>
2018-07-11 22:07:07 -0700

    add apple_pluralize.py

--- Python/PythonFundamentals/apple_pluralize.py
+++ Python/PythonFundamentals/apple_pluralize.py
@@ -1,17 +1,19 @@
 def pluralize(singular_arr):
     plural_arr = []
+    vowel = ["a", "i", "u", "e"]
     coronal = ["s", "r"]
     for singular in singular_arr:
-        if singular[-2:] == "ie":
-            if singular[-3] in coronal:
-                plural_arr.append(singular+"En")
-            else:
-                plural_arr.append(singular+"n")
-        else:
-            if singular[-1] in ["a", "i", "u"] and singular[-2:] != "ou":
-                plural_arr.append(singular+"'s")
-            else:
-                plural_arr.append(singular+"s")
+        if singular[-1:] in vowel: # for all vowel-ending singular forms,
+            if singular[-2:] == "ie": # if singular form ends in -ie,
+                if singular[-3] in coronal: # and it's preceded by a coronal,
+                    plural_arr.append(singular+"En") # -sieEn or -rieEn
+                else:
+                    plural_arr.append(singular+"n") # -ien
+            else: # if singular form does not end in -ie,
+                if singular[-1:] != "e" and singular[-2:] != "ou": # and it doesn't end in -e or -ou,
+                    plural_arr.append(singular+"'s") # -a's, -i's, -u's
+                else:
+                    plural_arr.append(singular+"s") # -es, -ous
     print plural_arr
 
 singular_arr = ["berie", "kasie", "karie", "sona", "soni", "sonu", "sonou", "sone"]
